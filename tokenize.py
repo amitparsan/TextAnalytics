39365eb669303ff287348edad60e8aa6bb49d62c
amitparsan <35518684+amitparsan@users.noreply.github.com>
2018-09-27 17:18:59 +0100

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -1,1 +1,4 @@
-
+from nltk.tokenize import word_tokenize, send_tokenize, stopwords
+data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
+words = print(word_tokenize(data))
+phrases = print(send_tokenize(data))
